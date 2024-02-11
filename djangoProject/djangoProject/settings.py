30c170028f060dc8c229b0a2c071147c4bf00413
Todd Trost <68034370+ToddTrost@users.noreply.github.com>
2024-02-11 16:29:10 -0600

    Update settings.py
    Added settings for hosting

--- djangoProject/djangoProject/settings.py
+++ djangoProject/djangoProject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ehu^))uqt89mu&7!g(awav$=h-6na0ctdu_w=7s5pv_5q)*beh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
